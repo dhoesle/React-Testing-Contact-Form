0875e6c5336a6f6933e54926e8e0f5a289f4799c
Daniel Hoesle <dhoesle21@gmail.com>
2020-07-09 17:01:41 -0600

    completed task 3

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -55,7 +55,7 @@ const ContactForm = () => {
           <textarea data-testid='message' id='message' name="message" ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre data-testid='result' style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,7 +2,24 @@ import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 import ContactForm from './ContactForm'
 
-test('ContactForm displays new contact to the screen', () => {
+// test('ContactForm can be filled out', () => {
+//     render(<ContactForm />)
+
+//     const firstNameInput = screen.getByLabelText(/first name/i)
+//     const lastNameInput = screen.getByLabelText(/last name/i)
+//     const emailInput = screen.getByLabelText(/email/i)
+//     const messageInput = screen.getByLabelText(/message/i)
+
+//     fireEvent.change(firstNameInput, { target: { value: 'Danny' } })
+//     console.log("firstNameInput", firstNameInput)
+//     fireEvent.change(lastNameInput, { target: { value: 'Burke' } })
+//     fireEvent.change(emailInput, { target: { value: 'bluebill1049@hotmail.com' }})
+//     fireEvent.change(messageInput, { target: { value: 'please work dear sweet god please work'}})
+    
+    
+// })
+
+test('ContactForm can submit', async () => {
     render(<ContactForm />)
 
     const firstNameInput = screen.getByLabelText(/first name/i)
@@ -17,7 +34,12 @@ test('ContactForm displays new contact to the screen', () => {
 
     const submitButton = screen.getByRole('button')
     fireEvent.click(submitButton)
+    
+    // const newPerson = screen.findByText(/junior/i)
 
-    const newPerson = screen.getByText(/danny/i)
+    const result = await screen.findByTestId('result')
+    console.log("result", result)
+    expect(result).toBeInTheDocument()
+    
 
 })
